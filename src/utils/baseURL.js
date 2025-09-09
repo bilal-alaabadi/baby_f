647f38a7cde7a42e100e3a7251dc952b8dc8ae75
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-09-09 12:21:45 +0400

    Update baseURL.js

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -2,5 +2,5 @@
 //     return 'https://www.alanthor.com';//رابط الباك اند بدل 5000
 // }
 export const getBaseUrl = () => {
-    return 'http://localhost:5000';//رابط الباك اند بدل 5000
+    return 'https://baby-b.vercel.app';//رابط الباك اند بدل 5000
 }
